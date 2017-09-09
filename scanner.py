f0e7367f90dd8dc876ec5a8781e82b39545f928e
tolgaparlan <tolgaparlan@gmail.com>
2017-09-09 20:01:38 +0200

    implemented threading

--- scanner.py
+++ scanner.py
@@ -1,41 +1,94 @@
 #!/usr/bin/env python3
 
-from multiprocessing import Pool
+import concurrent.futures
 import socket
 import re
 import time
+import sys
 
-def scan_port(port):
-	global address
+def parsePorts(raw_ports):
+	if(raw_ports == 'all'):
+		return [i for i in range(1,65537)]
+
+	splitted = raw_ports.split('-')
+
+	if(not len(splitted) == 2):
+		return None
+
 	try:
-		socket.create_connection((address, port), 0.2)
-		print(port)
+		begin = int(splitted[0])
+		end = int(splitted[1])
+	except ValueError:
+		return None 
+
+	if(end > 65536):
+		return None
+
+	return [i for i in range(begin,end+1)]
+
+
+def parse_IP(raw_IP):
+	if(not re.search('[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}',raw_IP)):
+		return None
+	return raw_IP
+
+def print_usage_and_terminate(filename):
+	usage = """[USAGE] python3 %s ip_address ports
+e.g. %s 10.10.10.10 all
+e.g. %s 10.10.10.10 1-1024
+port ranges are inclusive, and have to be between 1-65536
+	""" % (filename,filename,filename)
+	print(usage)
+	sys.exit()
+
+def scan_port(address, port):
+	try:
+		socket.create_connection((address, port), TIMEOUT)
+		return port		
 	except socket.timeout as e:
-		pass
+		return None
 	except Exception as e:
 		print(e)
+		return None
 
-begin = time.time()
 
-MAX_THREAD = 128
+if __name__ == '__main__':
+	begin = time.time()
 
-#get the address
-default = 'www.tureng.com'
-address = input("Enter the address to scan: ")
-if(not len(address)):
-	address = default
+	MAX_THREAD = 80
+	TIMEOUT = 0.2
 
-if(re.search('[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}',address)):
-	ip = address
-	print(socket.gethostbyaddr(address))
-else:
+	#get the address and the ports
+	filename = sys.argv[0]
 	try:
-		print(socket.gethostbyname(address))
-	except:
-		pass
+		raw_address = sys.argv[1]
+		raw_ports = sys.argv[2]
+	except IndexError:
+		print_usage_and_terminate(filename)
 
-print("Scanning")
-with Pool(MAX_THREAD//4) as pool:
-	pool.map(scan_port, range(1, 2**16 + 1))
+	address = parse_IP(raw_address)
+	if(address == None):
+		print_usage_and_terminate(sys.argv[0])
+		
+	ports = parsePorts(raw_ports)
+	if(ports == None):
+		print_usage_and_terminate(sys.argv[0])
 
-print("Time passed: " + str(time.time() - begin) + "seconds.")
+	print("Scanning %s ..." % address)
+
+	with concurrent.futures.ThreadPoolExecutor(max_workers=MAX_THREAD) as executor:
+		futures = []
+		open_port_counter = 0
+
+		for port in ports:
+			futures.append(executor.submit(scan_port,address,port))
+
+		for future in futures:
+			if(future.result()):
+				open_port_counter += 1
+				print(future.result())
+
+
+	# print(ports)
+	print("Open port(s) found: %d" % open_port_counter)
+	print("Time passed: " + "{0:0.2f}".format(time.time() - begin) + " seconds.")
